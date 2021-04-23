e0d39de70e8789003224b8d4530559e83c95e60b
Lize Wu <retniw72@gmail.com>
2021-04-23 17:17:47 +0800

    2021-0423 新增常見問答

---
+++ TTS/QA.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="zh-Hant">
+<?php require_once('include/inc_head.php'); ?>
+<body>
+<div class="wrapper">
+    <!-- header Start -->
+    <?php require_once('include/inc_header.php'); ?>
+    <!-- header End -->
+
+    <!-- inner-kv -->
+    <?php require_once('include/inc_inner-kv.php'); ?>
+
+    <!-- main Start -->
+    <div id="center" class="main innerpage tie-2021">
+        <div class="container">
+            <!-- <h2>主標題/h2> -->
+            <!-- QA -->
+            <div class="accordion">
+                <ul>
+                    <li>
+                        <a href="#" title="">如何註冊會員</a>
+                        <div class="accordion-content">
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis ab perferendis ipsum molestias, aliquam mollitia laboriosam <a href="#">項目2</a> officiis quidem ea accusantium quis aperiam assumenda sint in non voluptatum sequi error nesciunt.</p>
+                            <ol>
+                                <li>項目1</li>
+                                <li><a href="#">項目2</a></li>
+                                <li>項目3</li>
+                            </ol>
+                            <ul>
+                                <li>項目1</li>
+                                <li>項目2</li>
+                                <li>項目3</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" title="">第二項說明 Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Neque ad iusto ducimus, consequuntur consectetur nam iste explicabo sequi laudantium autem minus dolore enim ullam sit officia dignissimos quas excepturi saepe.</a>
+                       <div class="accordion-content">...</div>
+                    </li>
+                    <li>
+                        <a href="#" title="">第三項說明</a>
+                        <div class="accordion-content">...</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- main End -->
+
+<!-- footer -->
+<?php require_once('include/inc_footer.php'); ?>
+
+</div>
+<!-- wrapper End -->
+<a href="javascript:;" class="scrollToTop" title="回頁首">回頁首</a>
+<?php require_once('include/inc_jq.php'); ?>
+</body>
+
+</html>
